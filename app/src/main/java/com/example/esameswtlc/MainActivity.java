b954f8858eb800ab2eceac178a3647ba43dac677
Palma99 <palma99@icloud.com>
2020-11-20 19:10:54 +0100

    a caso

--- app/src/main/java/com/example/esameswtlc/MainActivity.java
+++ app/src/main/java/com/example/esameswtlc/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // Commento di prova
+        // Altro commento
     }
+
 }
