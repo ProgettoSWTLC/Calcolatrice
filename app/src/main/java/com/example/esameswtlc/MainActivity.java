6fd87e5677847f6d47ef4bfb8ef179e360b707db
Federico <federico.riccilucchi@outlook.it>
2020-11-21 01:06:26 +0100

    Prova

--- app/src/main/java/com/example/esameswtlc/MainActivity.java
+++ app/src/main/java/com/example/esameswtlc/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // Commento di prova
         // Altro commento
+        // Ciao
     }
 
 }
