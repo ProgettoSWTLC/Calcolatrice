5913b163ac23814377041b636fca08e818a077e2
Flucchi <federico.riccilucchi@outlook.it>
2020-11-25 11:44:26 +0100

    Update showHistory.java

--- app/src/main/java/com/example/esameswtlc/showHistory.java
+++ app/src/main/java/com/example/esameswtlc/showHistory.java
@@ -9,12 +9,16 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
 
 import java.util.ArrayList;
+
+import static com.example.esameswtlc.R.attr.fastScrollHorizontalTrackDrawable;
 
 public class showHistory extends AppCompatActivity {
 
@@ -36,7 +40,7 @@ public class showHistory extends AppCompatActivity {
             MaterialButton newOperationView = new MaterialButton(this, null, R.attr.borderlessButtonStyle);
             newOperationView.setText(operation);
             LinearLayout.LayoutParams parametri = new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.MATCH_PARENT,
                     LinearLayout.LayoutParams.WRAP_CONTENT
             );
             parametri.weight = 1.0f;
@@ -44,6 +48,9 @@ public class showHistory extends AppCompatActivity {
             newOperationView.setLayoutParams(parametri);
             newOperationView.setPadding(0, 0, 0, 10);
             newOperationView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20f);
+            newOperationView.setLines(1);
+            newOperationView.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_START);
+            newOperationView.setHorizontallyScrolling(true);
             // Aggiungo event listener
             newOperationView.setOnClickListener(v -> {
                 Intent resultIntent = new Intent();
